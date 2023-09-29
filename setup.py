ae4288f74d7234973d5769bb1bfcb4a442edd72b
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2023-09-29 16:14:45 +0200

    remove exchnagelib submodule

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-setup(name='surfagenda',
-	version='0.1',
-	description='Easy access to Exchange calendars',
-	url='https://github.com/baszoetekouw/SurfChange',
-	author='Bas Zoetekouw',
-	author_email='bas.zoetekouw@surfnet.nl',
-	license='APL2',
-	packages=['surfagenda'],
-	install_requires=['exchangelib','ordereddict','Flask'],
-	python_requires='>=3.4',
-	zip_safe=False,
+setup(
+    name="surfagenda",
+    version="0.1",
+    description="Easy access to Exchange calendars",
+    url="https://github.com/baszoetekouw/SurfChange",
+    author="Bas Zoetekouw",
+    author_email="bas.zoetekouw@surfnet.nl",
+    license="APL2",
+    packages=["surfagenda"],
+    install_requires=["exchangelib", "ordereddict", "Flask"],
+    python_requires=">=3.11",
+    zip_safe=False,
 )
